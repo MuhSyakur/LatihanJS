aa12888311213eeb1e579e66cb784092a13fc76a
Syakur <syakurmu@gmail.com>
2021-01-21 21:40:16 +0700

    Belajar Looping

---
+++ Looping/Looping.js
@@ -0,0 +1,14 @@
+var i = 2
+console.log("Looping Pertama")
+while(i <= 20){
+console.log(i + " I love Coding")
+i = i + 2
+}
+
+var i1 =  20
+console.log("Looping Kedua")
+while (i1  > 0 ){
+    console.log(i1 + " I love Coding")
+    i1 = i1 - 2
+}
+   
---
+++ Looping/Looping2.js
@@ -0,0 +1,7 @@
+var i1 =  20
+console.log("Looping Kedua")
+while (i1 > 0){
+    console.log(i1 + " I love Coding")
+    i1 = i1 - 2
+}
+   
